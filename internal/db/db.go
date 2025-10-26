eefa262f70cef1577c83e4e32a6a605ccb013e0d
ModderCodder <1modder.path1@gmail.com>
2025-10-26 22:31:34 +0300

    GORN_add

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,19 @@
 package db
+
+import (
+	"github.com/binarymodder/fitness-tracker-server/internal/graph/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDataBase() (*gorm.DB, error) {
+
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Russia/Krasnodar", PreferSimpleProtocol: true}), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(&model.Todo{})
+
+	return db, nil
+}
